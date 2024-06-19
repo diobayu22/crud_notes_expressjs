8560af7c4f2fa87e44336e1d054e68673051d397
diobayu <diobayupranata22@gmail.com>
2024-06-19 22:35:07 +0700

    first commit

---
+++ backend/models/NoteModel.js
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const Notes = db.define('notes', {
+    note: DataTypes.STRING,
+
+}, {
+    freezeTableName: true
+});
+
+export default Notes;
+
+(async () => {
+    await db.sync();
+})();
